98385841fee5ba657e3f4f0636013a06df5f211b
cesar-velazquez <velazquezortizcesar@gmail.com>
2023-09-19 17:56:37 -0600

    update footer

--- src/components/QuoteBox.jsx
+++ src/components/QuoteBox.jsx
@@ -32,7 +32,8 @@ export const QuoteBox = ({ handleChangeQuote, quote, phrasesCelebres, isModeSpac
             {isModeSpace ?
                 <footer className="footer">
                     <h4 className="author">Fuente: {quote.author}</h4>
-                    <svg                        
+                    <svg 
+                    className="svg"                       
                         width="301"
                         height="118"
                         viewBox="0 0 301 118"
